c9ccfcd3b5f0ede42a84946c9e8e1cd583dc727d
Jacherr <jwc2002@outlook.com>
2024-01-11 00:00:04 +0000

    add more debug tracers

--- assyst-gateway/src/parser/event_handlers/message_create.rs
+++ assyst-gateway/src/parser/event_handlers/message_create.rs
@@ -1,14 +1,23 @@
 use assyst_common::gateway::core_event::CoreEventSender;
+use tracing::debug;
 use twilight_model::gateway::payload::incoming::MessageCreate;
 
 /// Handle a [MessageCreate] event received from the Discord gateway.
-/// 
+///
 /// This function undertakes the following steps:
-/// 1. Checks if the message is of interest, i.e., was not sent by a bot user. If not, it returns prematurely.
-/// 2. Passes the message to the command parser, which then attempts to convert the message to a command for further processing.
+/// 1. Checks if the message is of interest, i.e., was not sent by a bot user. If not, it returns
+///    prematurely.
+/// 2. Passes the message to the command parser, which then attempts to convert the message to a
+///    command for further processing.
 pub async fn handle(event: MessageCreate, tx: CoreEventSender) {
     // ignore all bot and webhook messages
-    if event.author.bot || event.webhook_id.is_some() { return; }
+    if event.author.bot || event.webhook_id.is_some() {
+        debug!(
+            "MESSAGE_CREATE: message not of interest: {} author",
+            if event.author.bot { "bot" } else { "webhook" }
+        );
+        return;
+    }
 
     // call command parser here
 }
--- assyst-gateway/src/parser/event_handlers/message_update.rs
+++ assyst-gateway/src/parser/event_handlers/message_update.rs
@@ -1,18 +1,36 @@
 use assyst_common::gateway::core_event::CoreEventSender;
-use twilight_model::{gateway::payload::incoming::MessageUpdate, channel::{Message, message::MessageType}, util::Timestamp};
+use tracing::debug;
+use twilight_model::channel::message::MessageType;
+use twilight_model::channel::Message;
+use twilight_model::gateway::payload::incoming::MessageUpdate;
+use twilight_model::util::Timestamp;
 
 /// Handle a [MessageUpdate] event sent from the Discord gateway.
-/// 
+///
 /// Message updates are used to check the following:
 /// 1. A message was edited into a command, in which case execute that command,
-/// 2. A message was edited into a different command, in which case execute the new command and edit the response message,
+/// 2. A message was edited into a different command, in which case execute the new command and edit
+///    the response message,
 /// 3. A command message was edited to a non-command, in which case delete the old command response.
 pub async fn handle(event: MessageUpdate, tx: CoreEventSender) {
+    // ignore all bot and webhook messages
+    if event.author.clone().map(|a| a.bot).unwrap_or(false) || event.edited_timestamp.is_none() {
+        debug!(
+            "MESSAGE_UPDATE: message not of interest: {}",
+            if event.edited_timestamp.is_none() {
+                "message not edited"
+            } else {
+                "bot author"
+            }
+        );
+        return;
+    }
+
     match convert_message_update_to_message(event) {
         Some(message) => {
             // call command parser here
         },
-        None => {}
+        None => {},
     }
 }
 
@@ -59,6 +77,6 @@ fn convert_message_update_to_message(event: MessageUpdate) -> Option<Message> {
         webhook_id: None,
         components: vec![],
         thread: None,
-        role_subscription_data: None
+        role_subscription_data: None,
     })
 }
