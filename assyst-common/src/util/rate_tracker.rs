acb6e051ff53ba44e3c6bf96dffffe240540722a
Jacherr <jwc2002@outlook.com>
2024-01-25 16:06:36 +0000

    add rate tracker doc comment

--- assyst-common/src/util/rate_tracker.rs
+++ assyst-common/src/util/rate_tracker.rs
@@ -2,6 +2,10 @@ use std::time::Duration;
 
 use tokio::time::Instant;
 
+/// Struct to allow the tracking of how fast a value increases, or how fast a state changes.
+/// 
+/// For example, can be used to determine how frequently a command is ran over a time period,
+/// or the rate of events being received.
 pub struct RateTracker {
     tracking_length: Duration,
     samples: Vec<(usize, Instant)>,
@@ -15,17 +19,20 @@ impl RateTracker {
     }
 
     pub fn add_sample(&mut self, value: usize) {
+        // add new sample
         self.samples.push((i.uses as usize, Instant::now()));
         let mut to_remove = vec![];
         for (pos, entry) in self.samples.iter().enumerate() {
-            // remove old entries
+            // determine which entries are out of range
             if Instant::now().duration_since(entry.1).as_secs() > self.tracking_length {
                 to_remove.push(pos);
             }
         }
+        // ramove out of range entries
         for i in to_remove {
             self.samples.remove(i);
         }
+        // sort samples oldest to most recent
         self.samples.sort_by(|a, b| a.0.cmp(&b.0));
     }
 
