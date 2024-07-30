64bf9aa61824208b6850cba1b65780507b081a27
Jacherr <jwc2002@outlook.com>
2024-07-30 23:46:50 +0100

    dont post shard count to top.gg

--- assyst-core/src/rest/top_gg.rs
+++ assyst-core/src/rest/top_gg.rs
@@ -9,13 +9,12 @@ static ROUTE: LazyLock<String> = LazyLock::new(|| format!("https://top.gg/api/bo
 
 pub async fn post_top_gg_stats(assyst: ThreadSafeAssyst) -> anyhow::Result<()> {
     let guild_count = assyst.metrics_handler.guilds.get();
-    let shard_count = assyst.shard_count;
 
     assyst
         .reqwest_client
         .post(&*ROUTE)
         .header("authorization", &CONFIG.authentication.top_gg_token)
-        .json(&json!({ "server_count": guild_count, "shard_count": shard_count, "shards": [] }))
+        .json(&json!({ "server_count": guild_count }))
         .send()
         .await?
         .error_for_status()?;
