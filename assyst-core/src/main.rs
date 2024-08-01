968bf4ab90ce391a1c3c7b3ef49a8f2e1fc16d73
Jacherr <jwc2002@outlook.com>
2024-08-01 19:02:55 +0100

    trim token

--- assyst-core/src/main.rs
+++ assyst-core/src/main.rs
@@ -96,9 +96,15 @@ async fn main() {
         info!(
             "Patreon synchronisation disabled in config.dev.disable_patreson_synchronisation, will only load admins as patrons"
         );
-    } else {
-        init_patreon_refresh(std::fs::read_to_string(PATREON_REFRESH_LOCATION).unwrap_or_default()).await;
     }
+
+    init_patreon_refresh(
+        std::fs::read_to_string(PATREON_REFRESH_LOCATION)
+            .unwrap_or_default()
+            .trim()
+            .to_owned(),
+    )
+    .await;
 
     assyst.register_task(Task::new(
         assyst.clone(),
